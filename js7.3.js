f45bef8b7ac14cbed003b84fc6ac9b799ed17779
faroksalman <136682886+faroksalman@users.noreply.github.com>
2023-07-30 18:17:00 +0300

    first commit

---
+++ js7.3.js
@@ -0,0 +1,7 @@
+// Example usage:
+  const movies = [
+    { title: "Movie 1", genres: ["Action", "Drama"] },
+    { title: "Movie 2", genres: ["Drama", "Romance"] },
+    { title: "Movie 3", genres: ["Action", "Thriller"] }
+  ];
+  
